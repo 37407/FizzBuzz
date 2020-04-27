7744cf47d4a6d74ace93f320ab15323af783562d
37407 <16180129+37407@users.noreply.github.com>
2020-04-27 21:22:04 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var i in Enumerable.Range(1, 100)) Console.WriteLine("{0:#}{1:;;Fizz}{2:;;Buzz}", i % 3 == 0 || i % 5 == 0 ? 0 : i, i % 3, i % 5);
+        }
+    }
+}
